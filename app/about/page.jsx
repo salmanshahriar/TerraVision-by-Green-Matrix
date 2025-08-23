bacf0f4770b2ad92f879d5bac205b73be0222755
Salman Shahriar <salman@salmans-macbook-air.tail9f5d05.ts.net>
2025-08-23 23:26:13 +0600

    uploaded

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-2xl text-white font-bold">About page</h1>
+    <div className="flex items-center justify-center h-screen bg-white">
+      <h1 className="text-2xl font-bold">About page</h1>
     </div>
   );
 }
--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,48 +1,56 @@
 "use client";
 
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
-  const [activeLink, setActiveLink] = useState("Home");
   const router = useRouter();
+  const pathname = usePathname();
 
-  const handleLinkClick = (linkName, path) => {
-    setActiveLink(linkName);
-    router.push(path); 
+  const handleLinkClick = (path) => {
+    router.push(path);
   };
 
+  const baseTextColor = pathname === "/about" ? "text-black" : "text-white";
+
   return (
-    <nav className="absolute w-full backdrop-blur-md border-b border-white/5 shadow-sm z-10 ">
+    <nav className="absolute w-full backdrop-blur-md border-b border-white/5 shadow-sm z-10">
       <div className="flex items-center justify-between px-6 py-1 mx-auto">
         {/* Logo */}
         <div className="flex items-center">
           <div className="w-6 h-6 bg-blue-600 rounded-lg border border-white/10 flex items-center justify-center mr-2 shadow-lg">
             <div className="w-2 h-2 bg-white rounded-full opacity-90"></div>
           </div>
-          <h2 className="font-bold text-lg bg-white bg-clip-text text-transparent">
+          <h2
+            className={`font-bold text-lg bg-clip-text text-transparent ${
+              pathname === "/about" ? "bg-black" : "bg-white"
+            }`}
+          >
             EarthPulse
           </h2>
         </div>
 
         {/* Navigation Links */}
         <div className="flex items-center gap-1 px-2 justify-between">
+          {/* Home */}
           <button
-            onClick={() => handleLinkClick("Home", "/")}
+            onClick={() => handleLinkClick("/")}
             className={`px-4 py-1 rounded-lg text-sm font-medium transition-all duration-200 ${
-              activeLink === "Home"
-                ? "w-full bg-blue-600 hover:bg-blue-700 text-white font-medium px-3 rounded-md text-sm transition-colors duration-200 flex items-center justify-center space-x-2 border-t-2 border-l-2 border-blue-400 border-r border-b border-r-blue-800/60 border-b-blue-800/60 shadow-lg hover:shadow-xl relative"
-                : "text-white border-t-2 border-l-2 border-r border-b border-white/0"
+              pathname === "/"
+                ? "bg-blue-600 hover:bg-blue-700 text-white font-medium px-3 rounded-md flex items-center justify-center border-t-2 border-l-2 border-blue-400 border-r border-b border-r-blue-800/60 border-b-blue-800/60 shadow-lg hover:shadow-xl"
+                : `${baseTextColor} border-t-2 border-l-2 border-r border-b border-transparent`
             }`}
           >
             Home
           </button>
+
+          {/* About */}
           <button
-            onClick={() => handleLinkClick("About", "/about")}
+            onClick={() => handleLinkClick("/about")}
             className={`px-4 py-1 rounded-lg text-sm font-medium transition-all duration-200 ${
-              activeLink === "About"
-                ? "w-full bg-blue-600 hover:bg-blue-700 text-white font-medium px-3 rounded-md text-sm transition-colors duration-200 flex items-center justify-center space-x-2 border-t-2 border-l-2 border-blue-400 border-r border-b border-r-blue-800/60 border-b-blue-800/60 shadow-lg hover:shadow-xl relative"
-                : "text-white border-t-2 border-l-2 border-r border-b border-white/0"
+              pathname === "/about"
+                ? "bg-blue-600 hover:bg-blue-700 text-white font-medium px-3 rounded-md flex items-center justify-center border-t-2 border-l-2 border-blue-400 border-r border-b border-r-blue-800/60 border-b-blue-800/60 shadow-lg hover:shadow-xl"
+                : `${baseTextColor} border-t-2 border-l-2 border-r border-b border-transparent`
             }`}
           >
             About
